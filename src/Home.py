22603383517aa43de71e8134e3df0dbb7d9584c0
freeman745 <140989591+freeman745@users.noreply.github.com>
2023-10-26 21:59:20 +0900

    init

---
+++ src/Home.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+
+#Config
+st.set_page_config(layout="wide", page_icon="💬", page_title="Chat-Bot 🤖")
+
+
+#Title
+st.markdown(
+    """
+    <h2 style='text-align: center;'>Your data-aware assistant 🤖</h1>
+    """,
+    unsafe_allow_html=True,)
+
+st.markdown("---")
+
+
+#Description
+st.markdown(
+    """ 
+    <h5 style='text-align:center;'>I'm an intelligent chatbot created by combining 
+    the strengths of Langchain and Streamlit. I use large language models to provide
+    context-sensitive interactions. My goal is to help you better understand your data.
+    I support PDF, TXT, CSV, Youtube transcript 🧠</h5>
+    """,
+    unsafe_allow_html=True)
+st.markdown("---")
+
+
+#Pages
+st.subheader("🚀 Pages")
+st.write("""
+- **Chat**: General Chat on data (PDF, TXT,CSV) with a [vectorstore](https://github.com/facebookresearch/faiss) (index useful parts(max 4) for respond to the user) | works with [ConversationalRetrievalChain](https://python.langchain.com/en/latest/modules/chains/index_examples/chat_vector_db.html)
+- **Sheet** (beta): Chat on tabular data (CSV) | for precise information | process the whole file | works with [CSV_Agent](https://python.langchain.com/en/latest/modules/agents/toolkits/examples/csv.html) + [PandasAI](https://github.com/gventuri/pandas-ai) for data manipulation and graph creation
+- **Youtube**: Summarize YouTube videos with [summarize-chain](https://python.langchain.com/en/latest/modules/chains/index_examples/summarize.html)
+""")
+st.markdown("---")
+
